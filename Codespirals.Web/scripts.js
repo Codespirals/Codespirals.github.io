6f46edd16aee88f93a8c63dc4b4f1125fc12eeb2
Eli <elias.styner@gmail.com>
2024-08-16 23:08:10 +0200

    deleted now obsolete files

--- Codespirals.Web/scripts.js
+++
@@ -1,9 +0,0 @@
-function setYear() {
-	document.getElementById("currentYear").innerHTML = new Date().getFullYear();
-}
-
-function onWindowLoad() {
-	setYear();
-}
-
-window.onload = onWindowLoad();
