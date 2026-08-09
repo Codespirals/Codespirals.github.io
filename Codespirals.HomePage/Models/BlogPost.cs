749530e39941ad57e463bfc5638b6ca8c3057b54
Elias-Styner <elias.styner@gmail.com>
2026-08-09 23:30:54 +0200

    Add project files.

---
+++ Codespirals.HomePage/Models/BlogPost.cs
@@ -0,0 +1,7 @@
+﻿namespace Codespirals.HomePage;
+
+public class BlogPost
+{
+    public PostMetadata Metadata { get; set; } = new();
+    public string Text { get; set; } = string.Empty;
+}
---
+++ Codespirals.HomePage/Services/GitHubService.cs
@@ -0,0 +1,23 @@
+﻿using Codespirals.Solutions.ApiCaller;
+
+namespace Codespirals.HomePage;
+
+public interface IGitHubService
+{
+
+}
+
+public class GitHubService : IGitHubService
+{
+    private ApiCaller _apiCaller;
+
+    public GitHubService(IApiCallerFactory apiCallerFactory)
+    {
+        _apiCaller = apiCallerFactory.InitializeApiCaller("https://api.github.com/", userAgent: "codespirals");
+        _apiCaller.AddDefaultHeader("Accept", "application/vnd.github+json");
+        _apiCaller.AddDefaultHeader("Authorization", "Bearer <YOUR-TOKEN>");
+        _apiCaller.AddDefaultHeader("X-GitHub-Api-Version", "2026-03-10");
+    }
+
+
+}
