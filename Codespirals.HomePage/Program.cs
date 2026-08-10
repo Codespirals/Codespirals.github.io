53524c9ba38348b77b6de063f26ec359efb5f070
Elias-Styner <elias.styner@gmail.com>
2026-08-11 00:03:11 +0200

    working on github interaction having a cors issue

--- Codespirals.HomePage/Program.cs
+++ Codespirals.HomePage/Program.cs
@@ -10,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IContentService, ContentService>();
 builder.Services.AddApiCallerFactory();
+builder.Services.AddScoped<IGitHubService, GitHubService>();
 
 await builder.Build().RunAsync();
