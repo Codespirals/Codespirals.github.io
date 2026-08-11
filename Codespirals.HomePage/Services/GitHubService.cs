a8c4016b2912ba28ad59473620efef396f1a7738
Elias-Styner <elias.styner@gmail.com>
2026-08-12 00:01:14 +0200

    updated nav worked on github service

--- Codespirals.HomePage/Services/GitHubService.cs
+++ Codespirals.HomePage/Services/GitHubService.cs
@@ -14,12 +14,12 @@ public class GitHubService : IGitHubService
 
     public GitHubService(IApiCallerFactory apiCallerFactory)
     {
-        _apiCaller = apiCallerFactory.CreateApiCaller("https://api.github.com/", userAgent: "codespirals");
+        _apiCaller = apiCallerFactory.CreateApiCaller("https://api.github.com/", group:"", userAgent: "codespirals");
         _apiCaller.AddDefaultHeader("Accept", "application/vnd.github+json");
         _apiCaller.AddDefaultHeader("Authorization", $"Bearer {Environment.GetEnvironmentVariable("CODESPIRALS-HOMEPAGE-READ")}");
         _apiCaller.AddDefaultHeader("X-GitHub-Api-Version", "2026-03-10");
     }
 
     public async Task<ApiResult<IEnumerable<Repo>>> GetRepositories()
-        => await _apiCaller.Get<IEnumerable<Repo>>("orgs/Codespirals/repos");
+        => await _apiCaller.Get<IEnumerable<Repo>>("orgs/Codespirals", "repos");
 }
