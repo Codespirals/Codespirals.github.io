21aff12c6b7ac63dde8b11dafcd01843d110b609
Elias-Styner <elias.styner@gmail.com>
2026-08-14 00:20:50 +0200

    github documentation for unauthenticated api calls sucks. Just saying.

--- Codespirals.HomePage/Services/GitHubService.cs
+++ Codespirals.HomePage/Services/GitHubService.cs
@@ -17,7 +17,6 @@ public class GitHubService : IGitHubService
     {
         _apiCaller = apiCallerFactory.CreateApiCaller("https://api.github.com/", group: "", userAgent: "codespirals");
         _apiCaller.AddDefaultHeader("Accept", "application/vnd.github+json");
-        _apiCaller.AddDefaultHeader("Authorization", $"Bearer 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");
         _apiCaller.AddDefaultHeader("X-GitHub-Api-Version", "2026-03-10");
     }
 
